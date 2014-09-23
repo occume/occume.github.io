d5087ff53717184f7ee19a231a3dad3b2e406f93
d_jin <d_jin@ctrip.com>
2014-09-23 16:33:41 +0800

    add protobuf

---
+++ client/js/D3.net.protobuf.1.0.js
@@ -0,0 +1,11 @@
+(function (D3) {
+	"use strict";
+	
+	if (typeof dcodeIO === 'undefined' || !dcodeIO.ProtoBuf) {
+	    throw(new Error("ProtoBuf.js is not present. Please see www/index.html for manual setup instructions."));
+	}
+	// Initialize ProtoBuf.js
+	var ProtoBuf = dcodeIO.ProtoBuf;
+	var Game = ProtoBuf.loadProtoFile("./game.proto").build("Game");
+	
+}( window.D3 = window.D3 || {}));
